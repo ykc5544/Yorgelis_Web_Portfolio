1f836c167dcd6054b02afbc92b868cfcd379b073
justSass <yorgeliscastillo92@gmail.com>
2026-02-08 19:19:27 -0500

    feat(dom): added toggle button to skill section

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ const header = document.querySelector('header h1');
 header.addEventListener('click', () => {
     header.textContent = 'Hello, Welcome To My Portfolio!';
 });
+
+const toggleSkillSection = document.getElementById('tog-skill');
+const skills = document.querySelector('#skills ul');
+
+toggleSkillSection.addEventListener('click', () => {
+    skills.style.display = skills.style.display === 'none' ? 'block' : 'none';
+});
