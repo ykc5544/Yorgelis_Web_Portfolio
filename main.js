64ca3e65c86fe09dd1056facf764f27880469cd5
justSass <yorgeliscastillo92@gmail.com>
2026-02-15 18:26:05 -0500

    added collapsible Bootstrap accordion

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ header.addEventListener('click', () => {
     header.textContent = 'Hello, Welcome To My Portfolio!';
 });
 
+/*
 const toggleSkillSection = document.getElementById('tog-skill');
 const skills = document.querySelector('#skills ul');
 
 toggleSkillSection.addEventListener('click', () => {
     skills.style.display = skills.style.display === 'none' ? 'block' : 'none';
 });
+*/
 
 document.addEventListener('DOMContentLoaded', () => {
     const avatar = document.getElementById('avatar');
